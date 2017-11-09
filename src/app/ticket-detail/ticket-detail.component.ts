203f117addbdb62efdb9a5323a88727074ed2738
Michael Henderson <mvhenderson.4@icloud.com>
2017-11-09 11:31:10 -0600

    various changes

---
+++ src/app/ticket-detail/ticket-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-detail',
+  templateUrl: './ticket-detail.component.html',
+  styleUrls: ['./ticket-detail.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TicketDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
