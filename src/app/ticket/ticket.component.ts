32c21ee289f34a682c7bc817887be716ad2f4a28
Michael Henderson <mvhenderson.4@icloud.com>
2017-11-10 15:50:10 -0600

    style a bit

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,3 +1,4 @@
+import { Ticket } from './../Models/Ticket';
 import { TicketService } from './../ticket.service';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
@@ -11,11 +12,18 @@ export class TicketComponent implements OnInit {
 
   constructor(private ticketService: TicketService) { }
   tickets = [];
+  selectTicket: Ticket;
+  isATicketSelected: boolean = false; 
 
   ngOnInit() {
     this.getAll();
   }
   getAll() {
     this.ticketService.GetAll().subscribe(response => this.tickets = response);
+  }
+
+  selectedTicket(ticket: Ticket) {
+    this.selectTicket = ticket;
+    this.isATicketSelected = true;
   }
 }
